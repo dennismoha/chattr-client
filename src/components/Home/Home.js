8e911eb8d6d8fb70a0c902d3307bb9166e10dae1
kurotsubasa <kurotsubasa10@gmail.com>
2021-02-02 13:33:26 -0500

    Created Button for redirecting to projects page
    Modified home page to include button that redirects to projects

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,47 +1,62 @@
 import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
-import apiUrl from '../../apiConfig'
-import axios from 'axios'
+// import apiUrl from '../../apiConfig'
+// import axios from 'axios'
 
 const Home = props => {
-  const [createdProjectId, setCreatedProjectId] = useState(null)
+  const [projects, setProjects] = useState(false)
 
-  if (createdProjectId) {
-    return <Redirect to={`/projects/${createdProjectId}`} />
+  if (projects === true) {
+    return <Redirect to={'/projects/'} />
   }
 
-  const projectCreate = () => {
-    axios({
-      url: `${apiUrl}/projects`,
-      method: 'POST',
-      data: { 'project': {
-        'user1': `${props.user._id}`,
-        'user1Email': `${props.user.email}`
-      }
-      },
-      headers: {
-        'Authorization': `Bearer ${props.user.token}`
-      }
-    })
-      .then(res => setCreatedProjectId(res.data.project._id))
-      .catch(() => props.msgAlert({
-        heading: 'Couldnt Create Project',
-        message: 'Are you signed in?',
-        variant: 'danger'
-      }))
+  const projjects = () => {
+    setProjects(true)
   }
 
-  const projecter = (
-    <Button onClick={projectCreate} type='button'>
-          Create Project!
+  const mover = (
+    <Button onClick={projjects} type='button'>
+          View your Projects!
     </Button>
   )
+  // const [createdProjectId, setCreatedProjectId] = useState(null)
+  //
+  // if (createdProjectId) {
+  //   return <Redirect to={`/projects/${createdProjectId}`} />
+  // }
+  //
+  // const projectCreate = () => {
+  //   axios({
+  //     url: `${apiUrl}/projects`,
+  //     method: 'POST',
+  //     data: { 'project': {
+  //       'user1': `${props.user._id}`,
+  //       'user1Email': `${props.user.email}`
+  //     }
+  //     },
+  //     headers: {
+  //       'Authorization': `Bearer ${props.user.token}`
+  //     }
+  //   })
+  //     .then(res => setCreatedProjectId(res.data.project._id))
+  //     .catch(() => props.msgAlert({
+  //       heading: 'Couldnt Create Project',
+  //       message: 'Are you signed in?',
+  //       variant: 'danger'
+  //     }))
+  // }
+  //
+  // const projecter = (
+  //   <Button onClick={projectCreate} type='button'>
+  //         Create Project!
+  //   </Button>
+  // )
 
   return (
     <div>
       <h1 className="lay">Welcome to Chattr</h1>
-      <h6>{projecter}</h6>
+      <h6>{mover}</h6>
     </div>
   )
 }
